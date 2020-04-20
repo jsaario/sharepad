1e6cd795c8a4ab06223225b8f1eabbb64b619a0f
Joonas Saario <jsaario@gmail.com>
2020-04-20 11:33:28 +0300

    Initial commit.

---
+++ Sharepad/ShareCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShareCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
