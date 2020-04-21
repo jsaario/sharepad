d5ff7d3bd0961a588736ce32d33795dd57e9da50
Joonas Saario <jsaario@gmail.com>
2020-04-22 00:21:36 +0300

    Initial commit.

---
+++ Sharepad/ShareWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShareWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
